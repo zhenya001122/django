91fba0ab88fd26502a5fbd300cef9a87a5b1f5e7
zhenya <sluckltu@gmail.com>
2024-04-24 21:50:57 +0300

    34 lesson fix Husband model

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -20,6 +20,7 @@ class Women(models.Model):
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
     tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='wuman')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -57,3 +58,12 @@ class TagPost(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag', kwargs={'tag_slug': self.slug})
+
+
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+    m_count = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.name
