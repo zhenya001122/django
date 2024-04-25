fd96303abce50afc749a61c28ca12e3666ee84ba
zhenya <sluckltu@gmail.com>
2024-04-25 15:01:56 +0300

    35 lesson fix show_categories and show_all_tags

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,5 +1,6 @@
 from django import template
 import women.views as views
+from django.db.models import Count
 from women.models import Category, TagPost
 
 register = template.Library()
@@ -7,10 +8,10 @@ register = template.Library()
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('women/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
