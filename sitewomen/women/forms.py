ab6286488351dff16f0aff0b85c743dab07e9ed0
zhenya <sluckltu@gmail.com>
2024-05-03 20:01:13 +0300

    45 lesson add AddPostForm, fix views

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -3,9 +3,9 @@ from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    # slug = forms.SlugField(max_length=255, label='URL') слаг формируется автоматически по полю title
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
+    is_published = forms.BooleanField(required=False, initial=True, label='Статус')
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категории')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), empty_label='Не замужем', required=False, label='Муж')
