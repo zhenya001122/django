ec6d77587592171fadf9126393a74e560abdc5c5
zhenya <sluckltu@gmail.com>
2024-06-03 19:16:21 +0300

    68 add EMAIL_BACKEND in settings

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -141,3 +141,5 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "users.authentication.EmailAuthBackend",
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
